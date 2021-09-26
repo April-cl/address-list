0a41ccc6a7e262a297910bf3993622d3be02ef0d
April-cl <xcl8202@gmail.com>
2021-09-27 01:21:07 +0800

    实现 read 操作

---
+++ pages/list/list.vue
@@ -0,0 +1,29 @@
+<template>
+	<view>
+		<unicloud-db v-slot:default="{data, loading, error, options}" collection="contact">
+			<view v-if="error">{{error.message}}</view>
+			<view v-else>
+				<uni-list>
+					<uni-list-item v-for="item in data" :key="item.id" :title="item.name" :note="item.phone" link></uni-list-item>
+				</uni-list>
+			</view>
+		</unicloud-db>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
