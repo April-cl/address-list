ada588d80e1e442f8798762fb5ca4d821112e147
April-cl <xcl8202@gmail.com>
2021-09-27 01:41:07 +0800

    实现 create 操作

---
+++ pages/add/add.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<uni-easyinput v-model="item.name" placeholder="name"></uni-easyinput>
+		<uni-easyinput v-model="item.phone" placeholder="phone"></uni-easyinput>
+		<button type="default" @click="submit">提交</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				item: {
+					"name": "",
+					"phone": ""
+				}
+			}
+		},
+		methods: {
+			submit() {
+				const db = uniCloud.database()
+				db.collection('contact').add(this.item).then( e => {
+					console.log(e)
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
